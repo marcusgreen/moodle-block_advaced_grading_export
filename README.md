88cbd231d9c396c142d15d466d18931380de5fdc
unknown <marcusavgreen@gmail.com>
2016-01-11 21:24:39 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+####Advanced Grading Export Block 
+Do not install, this code does nothing useful at the moment
+Marcus Green 2015
+Export and import advanced grading sheets. Designed for the BTEC Grading form
+in a way that should make it easily extendable to other advanced grading methods.
---
+++ classes/btec_grading_export.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ 
+class btec_grading_export {
+       function __construct() {
+       print "btec_grading_export constructor \r\n";
+   }
+ 
+}
